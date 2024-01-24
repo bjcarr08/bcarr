4db5ea4c2641dd6734d1bee753eefbba6f7b9b64
bjcarr08 <brandyjcarr@gmail.com>
2024-01-24 15:16:38 -0500

    added if statement for lineBreak option (default is TRUE)

--- R/Clipboard.R
+++ R/Clipboard.R
@@ -7,8 +7,12 @@
 #'
 #' @param comment.char A character vector of length one containing a single character or an empty string. Use "" to turn off the interpretation of comments altogether.
 #' @export
-readWriteClip_addQuoteComma <- function(comment.char = "|"){
+readWriteClip_addQuoteComma <- function(lineBreak = T, comment.char = "|"){
   x <- clipr::read_clip_tbl(header = F, stringsAsFactors = T, comment.char = comment.char)
   x$new <- paste0('"', x$V1, '"')
-  clipr::write_clip(x$new, breaks = ",")
+  if(lineBreak == T){
+    clipr::write_clip(x$new, breaks = ",\n")
+  } else {
+    clipr::write_clip(x$new, breaks = ",")
+  }
 }
