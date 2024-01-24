b80ee8babac4f56638eb2c71c590aee6eecc2f5c
bjcarr08 <brandyjcarr@gmail.com>
2024-01-24 14:37:03 -0500

    updated details and examples for readWriteClip_addQuoteComma() function

--- R/Clipboard.R
+++ R/Clipboard.R
@@ -4,19 +4,20 @@
 #' Add Double Quotes & Comma Separator
 #'
 #' Read a column of data from clipboard & writes with added double quotes and comma separator
-#' @param comment.char A character vector of length one containing a single character or an empty string. Use "" to turn off the interpretation of comments altogether.
+#'
+#' @details
+#' SKIP THIS STEP (your input data should be saved to your clipboard before running the next line)
+#' The function will not output anything to the console
+#' The new data is saved to your clipboard
+#' Paste contents of clipboard in any program you like
+#' Here is a view of the new data that was saved to clipboard in the above example
+#'
 #' @examples
-#' # example code
-#'
-#' # SKIP THIS STEP (your input data should be saved to your clipboard before running the next line)
-#' clipr::write_clip(c("test1","test2","test3"))
-#'
-#' # This will not output anything to your console, the new data is saved to your clipboard
+#' clipr::write_clip(c("test1","test2","test3"), allow_non_interactive = T)
 #' readWriteClip_addQuoteComma()
-#'
-#' # Paste in anyprogram you like, here is a view of the new data saved to clipboard
 #' # "test1","test2","test3"
 #'
+#' @param comment.char A character vector of length one containing a single character or an empty string. Use "" to turn off the interpretation of comments altogether.
 #' @export
 readWriteClip_addQuoteComma <- function(comment.char = "|"){
   x <- clipr::read_clip_tbl(header = F, stringsAsFactors = T, comment.char = comment.char)
