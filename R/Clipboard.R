0ba8d35a0a90b5812afa9216aaa7db1e847c07e1
bjcarr08 <brandyjcarr@gmail.com>
2024-09-05 14:48:39 -0400

    fixed True/False errors to T/F

--- R/Clipboard.R
+++ R/Clipboard.R
@@ -8,14 +8,14 @@
 #' @param singleQuotes A True or False value. True tells the function to write new values wrapped in single quotes, False will wrap values in double quotes. Default is True.
 #' @details Parameter commentChar treats any string following "|" char in the copied data as a comment & the code will ignore it, prob won't ever need to change this value.
 #' @export
-readWriteClip_addQuoteComma <- function(lineBreak = True, commentChar = "|", singleQuotes = True){
-  x <- clipr::read_clip_tbl(header = False, stringsAsFactors = True, comment.char = commentChar)
-  if(singleQuotes == True){
+readWriteClip_addQuoteComma <- function(lineBreak = T, commentChar = "|", singleQuotes = T){
+  x <- clipr::read_clip_tbl(header = F, stringsAsFactors = T, comment.char = commentChar)
+  if(singleQuotes == T){
     x$new <- paste0("'", x$V1, "'")
   } else {
     x$new <- paste0('"', x$V1, '"')
   }
-  if(lineBreak == True){
+  if(lineBreak == T){
     clipr::write_clip(x$new, breaks = ",\n")
   } else {
     clipr::write_clip(x$new, breaks = ",")
