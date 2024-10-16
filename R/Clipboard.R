bad9c61e2bab001993ea6ec66276b250747299dc
bjcarr08 <brandyjcarr@gmail.com>
2024-10-16 12:13:32 -0400

    updated code to copy lucid filenames and paste in call miner

--- R/Clipboard.R
+++ R/Clipboard.R
@@ -8,16 +8,43 @@
 #' @param singleQuotes A True or False value. True tells the function to write new values wrapped in single quotes, False will wrap values in double quotes. Default is True.
 #' @details Parameter commentChar treats any string following "|" char in the copied data as a comment & the code will ignore it, prob won't ever need to change this value.
 #' @export
-readWriteClip_addQuoteComma <- function(lineBreak = T, commentChar = "|", singleQuotes = T){
+readWriteClip_addQuoteComma <- function(lineBreak = T, commentChar = "|", singleQuotes = T, callMiner = F){
   x <- clipr::read_clip_tbl(header = F, stringsAsFactors = T, comment.char = commentChar)
-  if(singleQuotes == T){
-    x$new <- paste0("'", x$V1, "'")
+  if(callMiner == T){
+    xNew <-  dplyr::mutate(x, V1 = stringr::str_remove(x$V1, "lucid/robGrahamEnterprises/"))
+    xNew <-  dplyr::mutate(xNew, V1 = stringr::str_remove(xNew$V1, ".mp3"))
+    xNew$new <- paste0(xNew$V1, ";")
+    xNew$new[nrow(xNew)] <-  stringr::str_remove(xNew$new[nrow(xNew)], ";")
+    clipr::write_clip(xNew$new, breaks = "")
   } else {
-    x$new <- paste0('"', x$V1, '"')
-  }
-  if(lineBreak == T){
-    clipr::write_clip(x$new, breaks = ",\n")
-  } else {
-    clipr::write_clip(x$new, breaks = ",")
+    if(singleQuotes == T){
+      x$new <- paste0("'", x$V1, "'")
+    } else {
+      x$new <- paste0('"', x$V1, '"')
+    }
+    if(lineBreak == T){
+      clipr::write_clip(x$new, breaks = ",\n")
+    } else {
+      clipr::write_clip(x$new, breaks = ",")
+    }
   }
 }
+
+### OLD (updated on 10-16-2024)
+# readWriteClip_addQuoteComma <- function(lineBreak = T, commentChar = "|", singleQuotes = T, callMiner = F){
+#   x <- clipr::read_clip_tbl(header = F, stringsAsFactors = T, comment.char = commentChar)
+#   if(calMiner == T){
+#     x$new <- paste0(x$V1, ";")
+#   } else {
+#       if(singleQuotes == T){
+#         x$new <- paste0("'", x$V1, "'")
+#       } else {
+#         x$new <- paste0('"', x$V1, '"')
+#       }
+#       if(lineBreak == T){
+#         clipr::write_clip(x$new, breaks = ",\n")
+#       } else {
+#         clipr::write_clip(x$new, breaks = ",")
+#       }
+#   }
+# }
